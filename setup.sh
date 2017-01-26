fdc43cc7e497b300a99a6a7a3ebab7e85f0bcafd
nbprithv <niranjan@ripple.com>
2017-01-25 17:06:33 -0800

    adding setup script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+_mydir="`pwd`"
+
+rm ~/Library/Application\ Support/Sublime\ Text\ 3/Preferences.sublime-settings
+ln -s $_mydir/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
